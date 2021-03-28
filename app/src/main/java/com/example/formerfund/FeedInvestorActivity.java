955376380bfdbfcec8e72ae5fda3a5ba01e9db9b
CR-Codings <CRdev151>
2021-03-28 21:26:51 +0530

    Commit Design

---
+++ app/src/main/java/com/example/formerfund/FeedInvestorActivity.java
@@ -0,0 +1,14 @@
+package com.example.formerfund;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedInvestorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_investor);
+    }
+}
---
+++ app/src/main/java/com/example/formerfund/Investor/FeedViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.formerfund.Investor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.formerfund.R;
+
+public class FeedViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_view);
+    }
+}
